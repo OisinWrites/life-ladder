c1cc23a6ac6d259650f9c93c6301886793c58c31
OisinWrites <oisin.banville@gmail.com>
2024-04-26 10:55:37 +0100

    Create borrowing  section, lift states to app.js, include taxcalc utility

--- life-ladder/src/App.js
+++ life-ladder/src/App.js
@@ -1,11 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import { handleFormattedDisplay } from './utilities/FormatNumber'
 
 import BorrowingCapacityCalculator from './components/BorrowingCapacityCalculator';
 
 function App() {
+  const [applicants, setApplicants] = useState(1);
+  const [firstTimeBuyer, setFirstTimeBuyer] = useState(null);
+  const [salary1, setSalary1] = useState(null);
+  const [salary2, setSalary2] = useState(null);
+  const [maxBorrowableAmount, setMaxBorrowableAmount] = useState(null);
+  const [displaySwap, setDisplaySwap] = useState(false);  // State to control display logic
+  const [displayWarning, setDisplayWarning] = useState(false);
+  const [estimatedPropertyValue, setEstimatedPropertyValue] = useState(0);
+
+  const handleHeaderClick = () => {
+    if (borrowingSectionComplete) {
+      setDisplaySwap(true);   // Allow swap if conditions are met
+      setDisplayWarning(false); // Ensure warning is not shown if conditions are met
+    } else {
+      setDisplayWarning(true); // Show warning if conditions are not met
+      setDisplaySwap(false);   // Prevent swap if conditions are not met
+    }
+  };
+
+  const handleToggleComplete = () => {
+    setDisplaySwap(false);
+    setDisplayWarning(false);
+  };
+
+  const borrowingSectionComplete = salary1 !== null &&
+                                   firstTimeBuyer !== null &&
+                                   (applicants === 1 || (applicants === 2 && salary2 !== null));
+
+  const multiplier = firstTimeBuyer === 'Yes' ? 4 : 3.5;
+
+  useEffect(() => {
+      let totalSalary = parseFloat(salary1) || 0;
+      if (applicants === 2) {
+          totalSalary += parseFloat(salary2) || 0;
+      }
+      const calculatedAmount = totalSalary * multiplier;
+      setMaxBorrowableAmount(calculatedAmount);
+  }, [salary1, salary2, applicants, multiplier]);
+  
+  useEffect(() => {
+    if (maxBorrowableAmount) {
+        const calculatedValue = (maxBorrowableAmount / 9) * 10;
+        setEstimatedPropertyValue(calculatedValue);
+    }
+    }, [maxBorrowableAmount]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick={handleHeaderClick}>
         <h1 className="display-1">Life Ladder</h1>
         <br></br>
         <h2>Mortgage and Savings Calculator</h2>
@@ -13,7 +62,22 @@ function App() {
       </header>
       <main>
       <div>
-      <BorrowingCapacityCalculator/>
+        <BorrowingCapacityCalculator
+          applicants={applicants}
+          setApplicants={setApplicants}
+          firstTimeBuyer={firstTimeBuyer}
+          setFirstTimeBuyer={setFirstTimeBuyer}
+          salary1={salary1}
+          setSalary1={setSalary1}
+          salary2={salary2}
+          setSalary2={setSalary2}
+          maxBorrowableAmount={maxBorrowableAmount}
+          setMaxBorrowableAmount={setMaxBorrowableAmount}
+          displaySwap={displaySwap}
+          displayWarning={displayWarning}
+          handleToggleComplete={handleToggleComplete}
+          estimatedPropertyValue={estimatedPropertyValue}
+        />
       </div>
       </main>
     </div>
--- life-ladder/src/components/borrowingCapacityCalculator.js
+++ life-ladder/src/components/borrowingCapacityCalculator.js
@@ -1,15 +1,120 @@
-import React from 'react';
-import '.././static/borrowing-style.css';
+import React, { useState, useEffect } from 'react';
+import '../static/borrowing-style.css';
+import { handleNumericChange, handleFormattedDisplay, handleFormattedDisplayTwoDecimal } from '../utilities/FormatNumber'
 
-const borrowingCapacityCalculator = ({
 
-}) => {
+const BorrowingCapacityCalculator = ({
+        applicants,
+        setApplicants,
+        firstTimeBuyer,
+        setFirstTimeBuyer, 
+        salary1, 
+        setSalary1, 
+        salary2, 
+        setSalary2,
+        maxBorrowableAmount,
+        setMaxBorrowableAmount,
+        displaySwap,
+        displayWarning,
+        handleToggleComplete,
+        estimatedPropertyValue 
+        }) => {
 
-return (
-    <div>
-        <h2>Borrowing Capacity Calculator</h2>
-    </div>
-);
+    const multiplier = firstTimeBuyer === 'Yes' ? 4 : 3.5;
+    const formattedMaxBorrowableAmount = maxBorrowableAmount !== null ? handleFormattedDisplayTwoDecimal(parseFloat(maxBorrowableAmount)) : null;
+    const formattedEstimatedPropertyValue = estimatedPropertyValue !== null ? handleFormattedDisplayTwoDecimal(parseFloat(estimatedPropertyValue)) : null;
+
+    const [showInput, setShowInput] = useState(false);
+
+    return (
+        <div>
+            {displaySwap ? ( 
+            <div>
+                <h2>Borrowing Capacity Calculator</h2>
+                <button onClick={handleToggleComplete}>Edit Section</button>
+                <p>Number of Applicants: {applicants}</p>
+                <p>{firstTimeBuyer === 'Yes' ? "First Time Buyer" : "Second Time Buyer"}</p>
+                <p>Max Borrowable Amount: {formattedMaxBorrowableAmount}</p>
+                <p>Property Value at max LTV of 90%: {formattedEstimatedPropertyValue}</p>
+            </div>                
+            ) : (
+            <div>
+                <h2>Borrowing Capacity Calculator</h2>
+                {displayWarning && <p>Complete this section before moving on.</p>}
+                
+                <div className='applicants'>
+                    <button
+                        className={applicants === 1 ? 'active' : ''}
+                        onClick={() => setApplicants(1)}
+                    >
+                        1 Applicant
+                    </button>
+                    <button
+                        className={applicants === 2 ? 'active' : ''}
+                        onClick={() => setApplicants(2)}
+                    >
+                        2 Applicants
+                    </button>
+                </div>
+                <div className='salaries'>
+                    <input
+                        type="text"
+                        placeholder="Applicant 1's Salary"
+                        value={handleFormattedDisplay(salary1)}
+                        onChange={(e) => handleNumericChange(e.target.value, setSalary1)}
+                        />
+                    {applicants === 2 && (
+                        <input
+                            type="text"
+                            placeholder="Applicant 2's Salary"
+                            value={handleFormattedDisplay(salary2)}
+                            onChange={(e) => handleNumericChange(e.target.value, setSalary2)}
+                        />
+                    )}
+                </div>
+                <div className='first-time-buyers'>
+                    <p>
+                        {applicants === 2 ? 'Are you both first-time buyers?' : 'Are you a first-time buyer?'}
+                    </p>
+                    <div>
+                        <button
+                            className={firstTimeBuyer === 'Yes' ? 'active' : ''}
+                            onClick={() => setFirstTimeBuyer('Yes')}
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className={firstTimeBuyer === 'No' ? 'active' : ''}
+                            onClick={() => setFirstTimeBuyer('No')}
+                        >
+                            No
+                        </button>
+                    </div>
+                </div>
+                <div className='multiplier'>
+                    <p>Mortgage Borrowing Multiplier: {multiplier}x</p>
+                </div>
+                <div className='max-borrowable'>
+                    <p>Max Borrowable Amount: {formattedMaxBorrowableAmount}</p>
+                    <p>Property Value at max LTV of 90%: {formattedEstimatedPropertyValue}</p>
+                </div>
+                <div className='manual-quote'>
+                    <button onClick={() => setShowInput(!showInput)}>
+                        {showInput ? 'Hide Input' : 'Update Max Borrowable Amount'}
+                    </button>
+                    {showInput && (
+                        <input
+                        type="text"
+                        value={handleFormattedDisplay(maxBorrowableAmount)} // Handle null by replacing it with an empty string for input
+                        onChange={(e) => handleNumericChange(e.target.value, setMaxBorrowableAmount)}
+                        placeholder="Enter new amount"
+                        />
+                    )}
+                </div>
+            </div>
+            )}
+        </div>
+    );
 };
 
-export default borrowingCapacityCalculator;
+export default BorrowingCapacityCalculator;
